e8a13c83a760623a3f208c15d9630998887533fc
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-08-19 16:31:12 +0200

    Add weather component

---
+++ resources/js/components/Weather.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>{{ currentTemp }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentTemp: "",
+    };
+  },
+  methods: {
+    getWeather() {
+      let url =
+        "http://api.openweathermap.org/data/2.5/weather?q=Warsaw&?units=metric&APPID=4bc257cab658657dc3e0067d35b1e9ee&lang=pl";
+      axios
+        .get(url)
+        .then((response) => {
+          this.currentTemp = response.data.main.temp;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    beforeMount() {
+      this.getWeather();
+    },
+  },
+};
+</script>
